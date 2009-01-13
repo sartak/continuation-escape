c771221494b42fe60001bec869f6cef6033dfff2
Shawn M Moore <sartak@gmail.com>
2009-01-12 19:46:27 -0500

    Need to count the *difference* in frames, not refer to the absolute frame number

--- lib/Continuation/Escape.pm
+++ lib/Continuation/Escape.pm
@@ -6,17 +6,20 @@ our @EXPORT = 'call_cc';
 
 use Scope::Upper 'unwind';
 
+sub _count_caller_level () {
+    my $i = 0;
+    1 while caller($i++);
+    return $i - 1; # discard the _count_caller_level stack frame
+}
+
 sub call_cc (&) {
     my $code = shift;
 
-    my $caller_level = do {
-        my $i = 0;
-        1 while caller($i++);
-        $i;
-    };
+    my $escape_level = _count_caller_level;
 
     my $escape_continuation = sub {
-        unwind @_ => $caller_level;
+        my $difference = _count_caller_level - $escape_level;
+        unwind @_ => $difference;
     };
 
     return $code->($escape_continuation);
