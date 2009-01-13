47bbb9cd7f9a8aac735fcf72c82769e20e59d622
Shawn M Moore <sartak@gmail.com>
2009-01-12 19:50:46 -0500

    Make sure trying to save a continuation and using it later blows up instead of returning to some unknown scope (omg)

--- lib/Continuation/Escape.pm
+++ lib/Continuation/Escape.pm
@@ -6,6 +6,8 @@ our @EXPORT = 'call_cc';
 
 use Scope::Upper 'unwind';
 
+our $CURRENTEST_CONTINUATION;
+
 sub _count_caller_level () {
     my $i = 0;
     1 while caller($i++);
@@ -18,10 +20,16 @@ sub call_cc (&) {
     my $escape_level = _count_caller_level;
 
     my $escape_continuation = sub {
+        if (!defined($CURRENTEST_CONTINUATION)) {
+            require Carp;
+            Carp::croak("Escape continuations are not usable outside of their original scope.");
+        }
+
         my $difference = _count_caller_level - $escape_level;
         unwind @_ => $difference;
     };
 
+    local $CURRENTEST_CONTINUATION = $escape_continuation;
     return $code->($escape_continuation);
 }
 
