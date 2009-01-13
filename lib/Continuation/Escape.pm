cb8123e9836d6ac76f0d489bc9750ebdb3d19410
Shawn M Moore <sartak@gmail.com>
2009-01-12 20:26:41 -0500

    Documentation

--- lib/Continuation/Escape.pm
+++ lib/Continuation/Escape.pm
@@ -41,3 +41,65 @@ sub call_cc (&) {
 
 1;
 
+__END__
+
+=head1 NAME
+
+Continuation::Escape - escape continuations (returning higher up the stack)
+
+=head1 SYNOPSIS
+
+    use Continuation::Escape;
+
+    my $ret = call_cc {
+        my $escape = shift;
+
+        # ...
+        sub {
+            # ...
+            $escape->(1 + 1);
+            # code never reached
+        };
+        # code never reached
+    };
+
+    $ret # 2
+
+=head1 NAME
+
+An escape continuation is a limited type of continuation that only allows you
+to jump back up the stack. Invoking an escape continuation is a lot like
+throwing an exception, however escape continuations do not necessarily indicate
+exceptional circumstances.
+
+B<The interface for context will probably change.> Right now when squeezing a
+list into scalar context, the B<last> element is used, instead of the usual
+squeezing into the number of elements in that list. In the meantime, just use
+the same context on the receiving end as the sending end.
+
+This module builds on Vincent Pit's excellent L<Scope::Upper> to give you a
+nicer interface to returning to outer scopes.
+
+=head1 CAVEATS
+
+Escape continuations are B<not> real continuations. They are not re-invokable
+(meaning you only get to run them once) and they are not savable (once the
+C<call_cc> block ends, calling the continuation it gave you is an error). This
+module goes to some length to ensure that you do not try to do either of these
+things.
+
+Real continuations in Perl would require a lot of work. But damn would they be
+nice. Does anyone know how much work would even be involved? C<:)>
+
+=head1 AUTHOR
+
+Shawn M Moore, C<sartak@bestpractical.com>
+
+=head1 THANKS TO
+
+Vincent Pit for writing the excellent L<Scope::Upper> which does B<two> things
+that I've wanted forever (escape continuations and localizing variables at
+higher stack levels).
+
+=cut
+
